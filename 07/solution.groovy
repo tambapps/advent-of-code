99ba33fd2cd50f5b4db81858e43f5c98cf751f54
Nelson Fonkoua <nfonkoua@weborama.com>
2022-12-07 14:19:18 +0100

    small code cleaning

--- 07/solution.groovy
+++ 07/solution.groovy
@@ -24,10 +24,9 @@ for (String line in new File('input.txt').readLines()) {
   }
 }
 
-int sum = DIRECTORIES.collect { it.size }
+println "Part 1: the sum of the total size of all directories with a size of at most 100000 is " + DIRECTORIES.collect { it.size }
     .findAll { it <= 100000 }
     .sum()
-println "Part 1: the sum of the total size of all directories with a size of at most 100000 is " + sum
 
 int availableSpace = 70000000 - root.size
 int spaceNeeded = 30000000 - availableSpace
@@ -46,11 +45,9 @@ class FileNode {
   void addChild(FileNode fileNode) {
     fileNode.parent = this
     children << fileNode
-    if (fileNode.size > 0) {
-      FileNode node = fileNode
-      while ((node = node.parent)) {
-        node.size += fileNode.size
-      }
+    FileNode node = fileNode
+    while ((node = node.parent)) {
+      node.size += fileNode.size
     }
   }
 }
