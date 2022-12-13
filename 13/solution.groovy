487df0c24e5525b3fb3798eb7256353a6645feeb
Nelson Fonkoua <nfonkoua@weborama.com>
2022-12-13 18:22:52 +0100

    used a class

--- 13/solution.groovy
+++ 13/solution.groovy
@@ -1,9 +1,9 @@
-List<Tuple2<List, List>> pairs = new File('input.txt').newReader().with { BufferedReader reader ->
-  List<Tuple2<List, List>> pairs = []
+List<Tuple2<Packet, Packet>> pairs = new File('input.txt').newReader().with { BufferedReader reader ->
+  List<Tuple2<Packet, Packet>> pairs = []
   String line
   while ((line = readLine())) {
-    List packet1 = new PacketParser(line).parse()
-    List packet2 = new PacketParser(readLine()).parse()
+    Packet packet1 = new PacketParser(line).parse()
+    Packet packet2 = new PacketParser(readLine()).parse()
     pairs << new Tuple2<>(packet1, packet2)
     readLine() // empty line separator
   }
@@ -11,61 +11,75 @@ List<Tuple2<List, List>> pairs = new File('input.txt').newReader().with { Buffer
 }
 
 int rightIndexSum = 0
-
-pairs.eachWithIndex { Tuple2<List, List> pair, int i ->
+pairs.eachWithIndex { Tuple2<Packet, Packet> pair, int i ->
   int number = i + 1
-  if (compare(pair.v1, pair.v2) <= 0) {
-    rightIndexSum+= number
+  if (pair.v1 < pair.v2) {
+    rightIndexSum += number
   }
 }
 
 println("Part 1: sum of indices of well ordered packet is $rightIndexSum")
 
 // flattening packet pairs
-List<List> allPackets = pairs.collectMany { it }
-def divider1 = [[2]]
-def divider2 = [[6]]
+List<Packet> allPackets = pairs.collectMany { it }
+def divider1 = new Packet([2])
+def divider2 = new Packet([6])
 allPackets.addAll([divider1, divider2])
 
-allPackets.sort { l1, l2 -> compare(l1, l2) }
+allPackets.sort(true)
 int index1 = allPackets.indexOf(divider1) + 1
 int index2 = allPackets.indexOf(divider2) + 1
 
 println("Part 2: decoder key is " + (index1 * index2))
 
-static int compare(List l1, List l2) {
-  int i = 0
-  while (i < l1.size() && i < l2.size()) {
-    def item1 = l1[i]
-    def item2 = l2[i]
-    if (item1 instanceof Integer && item2 instanceof Integer) {
-      int c = item1 <=> item2
-      if (c != 0) {
-        return c
-      }
-    }
+class Packet extends LinkedList implements Comparable<Packet> {
 
-    if (item1 instanceof Integer && item2 instanceof List) {
-      int c = compare([item1], item2)
-      if (c != 0) {
-        return c
-      }
-    }
-    if (item1 instanceof List && item2 instanceof Integer) {
-      int c = compare(item1, [item2])
-      if (c != 0) {
-        return c
-      }
-    }
-    if (item1 instanceof List && item2 instanceof List) {
-      int c = compare(item1, item2)
-      if (c != 0) {
-        return c
-      }
-    }
-    i++
+  Packet() {
   }
-  return l1.size() <=> l2.size()
+
+  Packet(Collection l) {
+    super(l)
+  }
+
+  @Override
+  int compareTo(Packet p) {
+    return compare(this, p)
+  }
+
+  static int compare(Packet l1, Packet l2) {
+    int i = 0
+    while (i < l1.size() && i < l2.size()) {
+      def item1 = l1[i]
+      def item2 = l2[i]
+      if (item1 instanceof Integer && item2 instanceof Integer) {
+        int c = item1 <=> item2
+        if (c != 0) {
+          return c
+        }
+      }
+
+      if (item1 instanceof Integer && item2 instanceof Packet) {
+        int c = compare(new Packet([item1]), item2)
+        if (c != 0) {
+          return c
+        }
+      }
+      if (item1 instanceof Packet && item2 instanceof Integer) {
+        int c = compare(item1, new Packet([item2]))
+        if (c != 0) {
+          return c
+        }
+      }
+      if (item1 instanceof Packet && item2 instanceof Packet) {
+        int c = compare(item1, item2)
+        if (c != 0) {
+          return c
+        }
+      }
+      i++
+    }
+    return l1.size() <=> l2.size()
+  }
 }
 
 class PacketParser {
@@ -75,8 +89,8 @@ class PacketParser {
     this.line = line
   }
 
-  List parse() {
-    List packet = []
+  Packet parse() {
+    Packet packet = new Packet()
     i++ // skipping [
     while (i < line.size()) {
       if (current.isInteger()) {
