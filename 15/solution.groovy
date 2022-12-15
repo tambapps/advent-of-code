0ec0848a580238e78221a9c1d78c4b6abc73634f
Nelson Fonkoua <tambue@hotmail.fr>
2022-12-15 21:54:24 +0100

    brute force part2

--- 15/solution.groovy
+++ 15/solution.groovy
@@ -17,8 +17,19 @@ for (def x in xMin..xMax) {
     impossibleBeaconPositions++
   }
 }
-
 println("Part 1: in the row where y=$yInterest, $impossibleBeaconPositions positions cannot contain a beacon")
+
+// part2
+def (min, max) = [0, 4000000]
+for (def x in min..max) {
+  for (def y in min..max) {
+    if (!sensorBeacons.any { x == it.xb && y == it.yb }
+        && !sensorBeacons.any { len2(x, y, it.xs, it.ys) <= it.len2 }) {
+      println("x=$x, y=$y")
+    }
+  }
+}
+
 static long len2(long x1, long y1, long x2, long y2) {
   def r= Math.abs(x2 - x1) + Math.abs(y2 - y1)
   if (r < 0) throw new RuntimeException("use bigger number class")
