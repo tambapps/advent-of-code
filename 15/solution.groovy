fce0f94bd64897b3769e1959c228895da716e43f
Nelson Fonkoua <tambue@hotmail.fr>
2022-12-15 21:36:11 +0100

    day 15 part 1

--- 15/solution.groovy
+++ 15/solution.groovy
@@ -1,26 +1,28 @@
 def sensorBeacons = new File('input.txt').readLines().collect { String line ->
   def (_, xs, ys, xb, yb) = (line =~ /Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/)[0]
   [xs: xs, ys: ys, xb: xb, yb: yb].tap {
-    replaceAll { key, value -> value.toInteger() }
+    replaceAll { key, value -> value.toLong() }
     len2 = len2(it.xs, it.ys, it.xb, it.yb)
   }
 }
 
 println(sensorBeacons)
-final int yInterest = 10
-List<Integer> xList = sensorBeacons.collectMany { [it.xs, it.xb] }
-final int xMin = xList.min()
-final int xMax = xList.max()
-println(xMax)
-
-int impossibleBeaconPositions = 0
+final long yInterest = 26
+final long xMin = sensorBeacons.collect() {it.xs - it.len2  }
+    .min()
+final long xMax = sensorBeacons.collect() {it.xs + it.len2  }.max()
+println("min=$xMin, max=$xMax")
+long impossibleBeaconPositions = 0
 for (def x in xMin..xMax) {
-  if (sensorBeacons.any {x != it.xb && yInterest != it.yb && len2(x, yInterest, it.xs, it.ys) <= it.len2 }) {
+  if (!sensorBeacons.any { x == it.xb && yInterest == it.yb }
+    && sensorBeacons.any { len2(x, yInterest, it.xs, it.ys) <= it.len2 }) {
     impossibleBeaconPositions++
   }
 }
 
 println(impossibleBeaconPositions)
-static int len2(int x1, int y1, int x2, int y2) {
-  return (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)
+static long len2(long x1, long y1, long x2, long y2) {
+  def r= Math.abs(x2 - x1) + Math.abs(y2 - y1)
+  if (r < 0) throw new RuntimeException("caca")
+  r
 }
