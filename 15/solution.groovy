fc55145cf654245ee476193ad0f2bb6738d8b7ce
Nelson Fonkoua <tambue@hotmail.fr>
2022-12-18 12:53:31 +0100

    renamed things

--- 15/solution.groovy
+++ 15/solution.groovy
@@ -1,27 +1,27 @@
 // execute it with --compile-static for better performance
-class SensorBeacon { long xs; long ys; long xb; long yb; long len2 }
+class SensorBeacon { long xs; long ys; long xb; long yb; long len }
 List<SensorBeacon> sensorBeacons = new File('input.txt').readLines().collect { String line ->
   List<Long> coordinates = (line =~ /-?\d+/).findAll().collect { it.toString().toLong() }
   new SensorBeacon(xs: coordinates[0], ys: coordinates[1], xb: coordinates[2], yb: coordinates[3]).tap {
-    len2 = len2(it.xs, it.ys, it.xb, it.yb)
+    len = len(it.xs, it.ys, it.xb, it.yb)
   }
 }
 
 final long yInterest = 2000000
-final long xMin = sensorBeacons.collect() {it.xs - it.len2  }.min()
-final long xMax = sensorBeacons.collect() {it.xs + it.len2  }.max()
+final long xMin = sensorBeacons.collect() {it.xs - it.len  }.min()
+final long xMax = sensorBeacons.collect() {it.xs + it.len  }.max()
 println("min=$xMin, max=$xMax")
 long impossibleBeaconPositions = 0
 for (def x in xMin..xMax) {
   if (!sensorBeacons.any { x == it.xb && yInterest == it.yb }
-    && sensorBeacons.any { len2(x, yInterest, it.xs, it.ys) <= it.len2 }) {
+    && sensorBeacons.any { len(x, yInterest, it.xs, it.ys) <= it.len }) {
     impossibleBeaconPositions++
   }
 }
 
 println("Part 1: in the row where y=$yInterest, $impossibleBeaconPositions positions cannot contain a beacon")
-static long len2(long x1, long y1, long x2, long y2) {
-  def r= Math.abs(x2 - x1) + Math.abs(y2 - y1)
+static long len(long x1, long y1, long x2, long y2) {
+  def r= Math.abs(x2 - x1) + Math.abs(y2 - y1) // Manhattan distance
   if (r < 0) throw new RuntimeException("use bigger number class")
   r
 }
