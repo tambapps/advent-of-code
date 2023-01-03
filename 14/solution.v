551fb6b1cfc6feb5dbc0189434c20e80b86fc9b4
Nelson Fonkoua <tambue@hotmail.fr>
2023-01-03 23:02:31 +0100

    more optimization

--- 14/solution.v
+++ 14/solution.v
@@ -2,59 +2,27 @@ import os { read_lines }
 import math { max, min }
 import arrays
 
-struct Point {
-  x int
-  y int
-}
-enum Type as u8 {
-  rock
-  empty
-}
-
-[inline]
-fn str(x int, y int) string {
-  return "(x=$x, y=$y)"
-}
-
- // TODO get rid of this struct
-struct Cave {
-  highest_y int [required]
-  with_ground bool
-mut:
-  cave map[string]Type = map[string]Type{}
-}
-
-fn (mut self Cave) put(x int, y int, t Type) {
-  self.cave[str(x, y)] = t
-}
-
-fn (self &Cave) get(x int, y int) Type {
-  return self.cave[str(x, y)] or {
-    if self.with_ground && y >= self.highest_y + 2 {
-      Type.rock
-    } else {
-      Type.empty
-    }
-  }
-}
+ const(
+   rock = true
+ )
 
 fn main() {
-  rocks := arrays.flat_map<string, Point>(read_lines('input.txt')!, parse)
-  highest_y := arrays.max(rocks.map(it.y))!
-  mut cave1 := map[string]Type{}
-  mut cave2 := map[string]Type{}
-  for rock in rocks {
-    cave1[str(rock.x, rock.y)] = Type.rock
-    cave2[str(rock.x, rock.y)] = Type.rock
+  rocks := arrays.flat_map<string, string>(read_lines('input.txt')!, parse)
+  highest_y := arrays.max(rocks.map(it.split(',')[1].int()))!
+  mut cave1 := map[string]bool{}
+  mut cave2 := map[string]bool{}
+  for rock_position in rocks {
+    cave1[rock_position] = rock
+    cave2[rock_position] = rock
   }
   mut count := fall_pt1(mut cave1, highest_y)
   println('Part 1: $count units of sand came to rest')
+
   count = fall_pt2(mut cave2, highest_y + 2)
   println('Part 2: $count units of sand came to rest')
 }
 
-// fall_pt1 return true if should not fall anymore after
-fn fall_pt1(mut cave map[string]Type, highest_y int) int {
+fn fall_pt1(mut cave map[string]bool, highest_y int) int {
   mut x := 500
   mut y := 0
   mut count := 0
@@ -68,7 +36,7 @@ fn fall_pt1(mut cave map[string]Type, highest_y int) int {
       x++
       y++
     } else { // couldn't go anywhere
-      cave[str(x, y)] = Type.rock
+      cave[str(x, y)] = rock
       x = 500
       y = 0
       count++
@@ -76,15 +44,14 @@ fn fall_pt1(mut cave map[string]Type, highest_y int) int {
   }
   return count
 }
-fn fall_pt2(mut cave map[string]Type, floor_y int) int {
-    // TODO not handling ground (and therefore not working) and try to optimize conditions
+fn fall_pt2(mut cave map[string]bool, floor_y int) int {
   mut x := 500
   mut y := 0
-  mut count := 0
+  mut count := 1 // because we need one more to know this is over
   mut new_drop := true
   for {
     if y + 1 == floor_y {
-      cave[str(x, y)] = Type.rock
+      cave[str(x, y)] = rock
       // dropping a new sand
       x = 500
       y = 0
@@ -104,7 +71,7 @@ fn fall_pt2(mut cave map[string]Type, floor_y int) int {
     } else if new_drop { // new_drop and couldn't go anywhere? this is over
       break
     } else { // couldn't go anywhere
-      cave[str(x, y)] = Type.rock
+      cave[str(x, y)] = rock
       // dropping a new sand
       x = 500
       y = 0
@@ -115,8 +82,11 @@ fn fall_pt2(mut cave map[string]Type, floor_y int) int {
   return count
 }
 
-// parsing stuff
-fn parse(line string) []Point {
+struct Point {
+  x int
+  y int
+}
+fn parse(line string) []string {
   line_ends := line.split(' -> ').map(fn (c string) Point {
     fields := c.split(',').map(it.int())
     return Point {
@@ -124,19 +94,20 @@ fn parse(line string) []Point {
       y: fields[1]
     }
   })
-  mut rocks := []Point{}
+  mut rocks := []string{}
   for i := 1; i < line_ends.len; i++ {
     start := line_ends[i - 1]
     end := line_ends[i]
     for x in min(start.x, end.x)..(max(start.x, end.x) + 1) {
       for y in min(start.y, end.y)..(max(start.y, end.y) + 1) {
-        rocks << Point {
-          x: x
-          y: y
-        }
+        rocks << str(x, y)
       }
     }
   }
   return rocks
 }
 
+[inline]
+fn str(x int, y int) string {
+  return "$x,$y"
+}
